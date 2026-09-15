ef1bc3f86855b30501307f5c822b5e97fcf700d7
thichantauhu <yuki985400@gmail.com>
2026-09-15 13:27:04 +0700

    Add client game configuration

---
+++ public/config.js
@@ -0,0 +1,13 @@
+window.GAME_CONFIG = {
+  name: 'Tên game',
+  minPlayers: 3,
+  maxPlayers: 7,
+  maxBots: 7,
+  rules: [
+    'Thay luật chơi tại đây.',
+    'Mỗi phòng có thể có nhiều người chơi.',
+    'Chủ phòng có thể thêm bot và kick người chơi.',
+    'Bấm Bắt đầu khi đủ số người tối thiểu.',
+    'Gameplay nằm trong public/game.html.'
+  ]
+};
